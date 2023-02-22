c0c90a385af5193bbe0ebe767357d59905995e9e
Issadeen <issadeenabdiali@outlook.com>
2023-02-22 12:54:19 -0800

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _abs - checks for absolute value
  *
- * Return: Always 0.
+ * @a: parameter to be checked
+ *
+ * Return: Always a.
  */
-int main(void)
-{
-int r;
 
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
 }
