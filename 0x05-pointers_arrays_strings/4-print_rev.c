df9687460a035ccf242d26bda5b5b4edebc4eddb
Issadeen <issadeenabdiali@outlook.com>
2023-02-28 03:40:10 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,16 @@
 */
 void print_rev(char *s)
 {
-    int len = 0;
-    int i;
+int len = 0;
+int i;
 
-    /* find the length of the string */
-    while (s[len] != '\0')
-        len++;
+/* find the length of the string */
+while (s[len] != '\0')
+len++;
 
-    /* print the string in reverse */
-    for (i = len - 1; i >= 0; i--)
-        putchar(s[i]);
+/* print the string in reverse */
+for (i = len - 1; i >= 0; i--)
+putchar(s[i]);
 
-    putchar('\n');
+putchar('\n');
 }
