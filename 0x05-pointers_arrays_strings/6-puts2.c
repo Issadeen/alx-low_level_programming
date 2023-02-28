9e45fe13d5597ef86bbe873b571810ddb8bcdd90
Issadeen <issadeenabdiali@outlook.com>
2023-02-28 13:04:19 -0800

    7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
 /**
-* puts2 - prints every other character of a string,
-* starting with the first character, followed by a new line.
-*
-* @str: pointer to the string to print.
-*
-* Return: void.
-*/
-
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * @str: pointer to the string to print.
+ *
+ * Return: void.
+ */
 void puts2(char *str)
 {
-int i = 0;
+    int i;
 
-for (i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-i++;
-}
-_putchar('\n');
+    for (i = 0; str[i] != '\0'; i += 2)
+        _putchar(str[i]);
+
+    _putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* puts_half - prints half of a string, followed by a new line
+* @str: pointer to the string to be printed
+*
+* Return: void
+*/
+void puts_half(char *str)
+{
+int length = 0, i;
+
+while (str[length] != '\0')
+length++;
+
+if (length % 2 == 0)
+i = length / 2;
+else
+i = (length + 1) / 2;
+
+for (; i < length; i++)
+_putchar(str[i]);
+
+_putchar('\n');
+}
