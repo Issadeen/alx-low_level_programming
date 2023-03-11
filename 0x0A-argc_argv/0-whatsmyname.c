1514dc2f57e47879f77bd12f7a8f41b0e1ec028c
Issadeen <issadeenabdiali@outlook.com>
2023-03-11 01:23:54 -0800

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Prints the name of the program
+* @argc: The number of arguments passed to the program
+* @argv: An array of pointers to the arguments
+*
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+(void)argc; /* Unused variable */
+printf("%s\n", argv[0]);
+return (0);
+}
