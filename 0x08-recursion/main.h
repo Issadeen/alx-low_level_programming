8a9245e91eca1358c85bb8ab4b20eac802a2441a
root <issadeenabdiali@outlook.com>
2023-06-08 13:07:42 -0700

    recursions

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype */
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+/* Other necessary includes or function prototypes */
+
+#endif /* MAIN_H */
