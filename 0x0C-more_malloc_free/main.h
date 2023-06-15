71c95e8b92eaeb0f5965f916efa0cae3fd43c104
root <issadeenabdiali@outlook.com>
2023-06-15 11:19:42 -0700

    mallocfree

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
