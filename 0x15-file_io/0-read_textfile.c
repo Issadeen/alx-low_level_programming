c0a13efc3ab04e90d7cf82605efc54a48476509e
root <issadeenabdiali@outlook.com>
2023-07-13 13:17:04 -0700

    mixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,48 +7,47 @@
 * @filename: The name of the file to read
 * @letters: The number of letters to read and print
 *
-* Return: The actual number of letters read and printed
+* Return: The actual number of letters read
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 if (filename == NULL)
-return (0);
+return 0;
 
 FILE *file;
+char *buffer;
+ssize_t bytes_read, bytes_written;
+
 file = fopen(filename, "r");
 if (file == NULL)
-return (0);
+return 0;
 
-char *buffer;
 buffer = malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL)
 {
 fclose(file);
-return (0);
+return 0;
 }
 
-ssize_t bytes_read;
 bytes_read = fread(buffer, sizeof(char), letters, file);
 if (bytes_read < 0)
 {
 free(buffer);
 fclose(file);
-return (0);
+return 0;
 }
 
-ssize_t bytes_written;
 bytes_written = fwrite(buffer, sizeof(char), bytes_read, stdout);
 if (bytes_written < bytes_read)
 {
 free(buffer);
 fclose(file);
-return (0);
+return 0;
 }
 
 free(buffer);
 fclose(file);
 
-return (bytes_written);
+return bytes_written;
 }
-
