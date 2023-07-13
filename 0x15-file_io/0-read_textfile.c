62e967048df48551e7400bb2e929a610d31511b8
root <issadeenabdiali@outlook.com>
2023-07-13 14:57:43 -0700

    000

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,48 @@
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
 * read_textfile - Reads a text file and prints it to the POSIX standard output
-* @filename: The name of the file to read
-* @letters: The number of letters to read and print
+*  @filename : Name of the file to read.
+* @letters: Number of letters to read and print.
 *
-* Return: The actual number of letters read
+* Return: The actual number of letters read and printed. 0 on failure.
 */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-if (filename == NULL)
-return 0;
-
-FILE *file;
+ssize_t n;
 char *buffer;
-ssize_t bytes_read, bytes_written;
+FILE *file;
+
+if (filename == NULL)
+return (0);
 
 file = fopen(filename, "r");
 if (file == NULL)
-return 0;
+return (0);
 
 buffer = malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL)
 {
 fclose(file);
-return 0;
+return (0);
 }
 
-bytes_read = fread(buffer, sizeof(char), letters, file);
-if (bytes_read < 0)
+n = fread(buffer, sizeof(char), letters, file);
+if (n == -1)
 {
-free(buffer);
 fclose(file);
-return 0;
+free(buffer);
+return (0);
 }
 
-bytes_written = fwrite(buffer, sizeof(char), bytes_read, stdout);
-if (bytes_written < bytes_read)
-{
-free(buffer);
+buffer[n] = '\0';
+printf("%s", buffer);
+
 fclose(file);
-return 0;
+free(buffer);
+
+return (n);
 }
 
-free(buffer);
-fclose(file);
-
-return bytes_written;
-}
