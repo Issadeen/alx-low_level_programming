c37ed9904f8ae99250ab6d94c88abb1091adcef2
root <issadeenabdiali@outlook.com>
2023-07-14 11:08:41 -0700

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,7 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+int main(int argc, char *argv[]);
+
 #endif /* MAIN_H */
 
